d0e1e8cc77436e15b316ac9c1f5f31cf027162e4
Carlos A. Crespo <lvccrespo@gmail.com>
2022-02-03 12:55:20 -0300

    change conf.py and exported latex files

--- conf.py
+++ conf.py
@@ -81,7 +81,7 @@ locale_dirs = ['../locales', 'cpython/locales']  # relative to the sourcedir
 # So, we put all the documentation as a single file for now.
 _stdauthor = r'Guido van Rossum\\and the Python development team'
 latex_documents = [
-    ('contents', 'python-docs-es.tex', u'Documentación de Python en Español',
+    ('tutorial/index', 'python-docs-es.tex', u'Documentación de Python en Español',
      _stdauthor, 'manual'),
 ]
 
